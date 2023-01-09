8e9baf42f95eff2d399e41da1afcc1c1b2d8e902
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2023-01-09 23:04:37 +0800

    Add files via upload

---
+++ sepet.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#script by Abi Darwish https://t.me/abidarwish
+
+VERSIONNAME="Sepet v"
+VERSIONNUMBER="1.0"
+GREEN="\e[1;32m"
+RED="\e[;31m"
+WHITE="\e[1m"
+NOCOLOR='\e[0m'
+MCKP_SOURCE_CODE=""
+DICTIONARY="dictionary.txt"
+
+function header() {
+    echo -e ${GREEN}" ${VERSIONNAME}${VERSIONNUMBER}" ${NOCOLOR}
+	echo -e " by ${WHITE}Abi Darwish${NOCOLOR}"
+}
+
+function translatorEngine() {
+    while IFS= read -r LINE; do
+        CHINESE_LANG=$(echo ${LINE} | awk -F= '{print $1}')
+        ENGLISH_LANG=$(echo ${LINE} | awk -F= '{print $2}')
+        sed -i "s/${CHINESE_LANG}/${ENGLISH_LANG}/" "${MCKP_SOURCE_CODE}"
+    done <"${DICTIONARY}"
+}
+
+function translate() {
+    clear
+    header
+    echo
+    read -p " Source code filename: " MCKP_SOURCE_CODE
+    [[ -z ${MCKP_SOURCE_CODE} ]] && translate
+    echo -e -n " Translating..."
+    translatorEngine 2>&1
+    echo -e ${GREEN}"done"${NOCOLOR}
+    echo
+    exit 0
+}
+
+translate
