810f4ddf6034ffe21d036286cb99242b1a7bc73d
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2023-01-10 12:52:01 +0800

    Update sepet.sh

--- sepet.sh
+++ sepet.sh
@@ -10,29 +10,32 @@ NOCOLOR='\e[0m'
 DICTIONARY="dictionary.txt"
 
 function header() {
-    echo -e ${GREEN}" ${VERSIONNAME}${VERSIONNUMBER}" ${NOCOLOR}
+	echo -e ${GREEN}" ${VERSIONNAME}${VERSIONNUMBER}" ${NOCOLOR}
 	echo -e " by ${WHITE}Abi Darwish${NOCOLOR}"
 }
 
 function translatorEngine() {
-    while IFS= read -r LINE; do
-        CHINESE_LANG=$(echo ${LINE} | awk -F= '{print $1}')
-        ENGLISH_LANG=$(echo ${LINE} | awk -F= '{print $2}')
-        sed -i "s/${CHINESE_LANG}/${ENGLISH_LANG}/" "${MCKP_SOURCE_CODE}"
-    done <"${DICTIONARY}"
+	while IFS= read -r LINE; do
+		CHINESE_LANG=$(echo ${LINE} | awk -F= '{print $1}')
+		ENGLISH_LANG=$(echo ${LINE} | awk -F= '{print $2}')
+		sed -i "s/${CHINESE_LANG}/${ENGLISH_LANG}/" "${MCKP_SOURCE_CODE}"
+	done <"${DICTIONARY}"
 }
 
 function translate() {
-    clear
-    header
-    echo
-    read -p " Source code filename: " MCKP_SOURCE_CODE
-    [[ -z ${MCKP_SOURCE_CODE} ]] && translate
-    echo -e -n " Translating..."
-    translatorEngine 2>&1
-    echo -e ${GREEN}"done"${NOCOLOR}
-    echo
-    exit 0
+	clear
+	header
+	echo
+	read -p " Source code filename: " MCKP_SOURCE_CODE
+	[[ -z ${MCKP_SOURCE_CODE} ]] && translate
+	echo -e -n " Translating..."
+	rm -rf dictionary.txt
+	wget -q -O dictionary.tmp "https://raw.githubusercontent.com/abidarwish/sepet/main/dictionary.txt"
+	cat dictionary.tmp | sort | uniq >dictionary.txt
+	translatorEngine 2>&1
+	echo -e ${GREEN}"done"${NOCOLOR}
+	echo
+	exit 0
 }
 
 translate
