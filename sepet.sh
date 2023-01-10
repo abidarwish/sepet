5338fbdecf1c7c98d6f26287d83beb3739f03040
Abi Darwish <42401100+abidarwish@users.noreply.github.com>
2023-01-10 23:33:54 +0800

    Update sepet.sh

--- sepet.sh
+++ sepet.sh
@@ -41,7 +41,7 @@ function restore() {
 	header
 	echo
 	read -p " Do you want to restore MCKP back to the original Chinese?[y/n]: " RESTORE
-	[[ ${RESTORE,,} != "y" ]] && translate
+	[[ ${RESTORE} != "y" ]] && translate
 	echo -e -n " Restoring..."
 	mv /koolshare/webs/Module_merlinclash.asp.bak /koolshare/webs/Module_merlinclash.asp
 	sleep 3
